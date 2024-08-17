7e698015ab5460fe5171bd57bac71746ad267043
rsaniceto14 <raphaelaniceto14@gmail.com>
2024-08-17 17:33:26 -0300

    [feat] add Google login

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,18 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
+// import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { quickSearchOptions } from "../_constants/search"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SideBarSheet = () => {
   return (
@@ -13,20 +21,45 @@ const SideBarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="text-lg font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button className="gap-1 font-bold" variant="outline">
+              <Image
+                src="./Google.svg"
+                width={16}
+                height={16}
+                alt="Fazer login com Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1599566147214-ce487862ea4f?q=80&w=2147&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Avatar>
+        </Avatar> */}
 
-        <div>
+        {/* <div>
           <p className="font-bold">Raphael Aniceto</p>
-          <p className="text-xs">raphaelaniceto14@gmail.com</p>
-          {/* <div className="flex flex-col ml-3">
+          <p className="text-xs">raphaelaniceto14@gmail.com</p> */}
+        {/* <div className="flex flex-col ml-3">
               <span className="font-bold">Raphael Aniceto</span>
               <span className="text-sm text-gray-400">
 
               </span> */}
-        </div>
+        {/* </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid p-5 py-5">
